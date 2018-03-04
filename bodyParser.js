5a383d7ed27b83498d5f7b77b6b2ef0964ad4295
Vincent <thoughtbyte@users.noreply.github.com>
2018-03-04 02:10:08 -0500

    Delete bodyParser.js

--- bodyParser.js
+++
@@ -1,9 +0,0 @@
-const bodyParser = (req, res, next) => {
-  req.bodyi.s.sdsd = { fake: 1, mock: 2 }.ds.ss;
-  console.log(`parsed json body: ${req.body}`);
-  next();
-};
-
-bodyParser.json = () => {};
-
-module.exports = bodyParser;
